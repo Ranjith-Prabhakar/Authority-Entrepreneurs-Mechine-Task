24cceb3ccd19ae827928ebcd437dafa386a83c6f
Ranjith <ranjithp2294@gmail.com>
2025-07-02 11:52:35 +0530

    searching has been implemented

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,6 +1,7 @@
 import logoImage from "../assets/logo.png";
 import NavBar from "../components/layout/NavBar";
-import SearchBar from "../components/layout/SearchBar";
+// import SearchBar from "../components/layout/SearchBar";
+import SearchBar from "../features/SearchBar/SearchBar";
 import "./header.css";
 export default function () {
   return (
